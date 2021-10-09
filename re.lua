370489b4762e9641d237e49e0e123f3c48d9bf0f
samr <samr.stuff@pm.me>
2021-10-08 18:53:56 -0700

    added re.choose

--- re.lua
+++ re.lua
@@ -1,22 +1,23 @@
 --[[
-    ReFile, Lua ReFile, lua re, or re, are made by samrcode on GitHub.
+    ReFile, Lua ReFile, or re, are made by samrcode on GitHub.
     You are allowed to change this code if you give credit to the original author.
 ]]
 
 local re = {}
 
 function re.math(op, x, y)
-    if (op == "+") then
+    if (op == 1) then
         print(x + y)
-    elseif (op == "-") then
+    elseif (op == 2) then
         print(x - y)
-    elseif (op == "*") then
+    elseif (op == 3) then
         print(x * y)
-    elseif (op == "/") then
+    elseif (op == 4) then
         print(x / y)
     else
         print("re: math")
-        print("| Something happened. Try again later.")
+        print("  | Something happened. Try again later.")
+		print("  | ERROR: " .. op .. " IS NOT A VALID OPERATOR. MIN IS 1. MAX IS 4.")
     end
 end
 
@@ -28,6 +29,50 @@ function re.pause(message)
         print(message)
         io.read(1)
     end
+end
+
+function re.choose(num, opt1, opt2, opt3, command1, command2, command3, errmsg)
+    if (num == 1) then
+        print("[1] " .. opt1)
+        local uin = io.read("*n")
+        if (uin == 1) then
+            os.execute(command1)
+        else
+            print(errmsg .. "Press Enter to continue.")
+            io.read()
+        end
+	elseif (num == 2) then
+		print("[1] " .. opt1)
+		print("[2] " .. opt2)
+		local uin = io.read("*n")
+		if (uin == 1) then
+			os.execute(command1)
+		elseif (uin == 2) then
+			os.execute(command2)
+		else
+			print(errmsg .. "Press Enter to continue.")
+			io.read()
+		end
+	elseif (num == 3) then
+		print("[1] " .. opt1)
+		print("[2] " .. opt2)
+		print("[3] " .. opt3)
+		local uin = io.read("*n")
+		if (uin == 1) then
+			os.execute(command1)
+		elseif (uin == 2) then
+			os.execute(command2)
+		elseif (uin == 3) then
+			os.execute(command3)
+		else
+			print(errmsg .. "Press Enter to continue.")
+			io.read()
+		end
+	else
+		print("re: chooose")
+		print("  | Something happened. Try again later.")
+		print("  | ERROR: " .. num .. " IS NOT A VALID AMOUNT. MIN IS 1. MAX IS 3.")
+	end
 end
 
 return re
